a4e3ce0f7753595ecc0441fd72aeb5d2b6db01b4
jyoti-sumeru <jyoti.singh@sumerusolutions.com>
2016-09-26 20:29:39 +0530

    Added Code for Inserting data

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,9 +2,21 @@ class HomesController < ApplicationController
   def index
     @positions = Position.all
     @trainings = Training.all
+    @selections = TrainingPosition.all
   end
 
   def create
+    trainings_params.each do |data|
+     data.to_h.each_pair do |content, index|
+       Training.find_by_name(content).positions <<  Position.where(:id => index)
+     end
+    end
+    redirect_to root_path
+  end
 
+  private
+
+  def trainings_params
+    params.slice(:trainings).permit![:trainings]
   end
 end
